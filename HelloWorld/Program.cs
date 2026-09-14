197b4974d604787c2f9a2e85a77cba0a52f05237
Alexandraa19 <sperehrest3@gmail.com>
2026-09-14 21:36:00 +0300

    Step 7: variables and data types

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,3 +1,49 @@
-﻿Console.WriteLine("Привет от ИСП-242!");
-Console.WriteLine("Автор: Перехрест Александра");
-Console.WriteLine("Год: 2026");
+﻿
+
+// string myName = "Саша";
+// int myAge = 18;
+// string myGroup = "ИСП-242";
+
+// Console.WriteLine($"Меня зовут {myName}, мне {myAge} лет, я учусь в группе {myGroup}.");
+
+
+// Console.Write("Введите ваше имя: ");
+// string name2 = Console.ReadLine();
+// Console.WriteLine($"Привет, {name2}!");
+
+// Console.Write("Введите ваш возраст: ");
+// string input = Console.ReadLine();
+
+// int age2 = int.Parse(input); 
+// Console.WriteLine($"Через 10 лет вам будет {age2 + 10} лет.");
+
+
+// int x = 10;
+// int y = 3;
+// Console.WriteLine(x + y); 
+// Console.WriteLine(x - y); 
+// Console.WriteLine(x * y); 
+// Console.WriteLine(x / y); 
+// Console.WriteLine(x % y); 
+
+string firstName = "Саша";
+string lastName = "Перехрест";
+string group = "ИСП-242";
+int birthYear = 2008;
+double gpa = 4.8;
+bool hasScholarship = true;
+int currentYear = 2026;
+int age = currentYear - birthYear;
+
+Console.WriteLine("Студенческое удостоверение");
+Console.WriteLine($"Имя: {firstName} {lastName}");
+Console.WriteLine($"Группа: {group}");
+Console.WriteLine($"Возраст: {age} лет");
+Console.WriteLine($"Средний балл: {gpa}");
+Console.WriteLine($"Стипендия: {hasScholarship}");
+
+
+Console.Write("Введите ваш любимый предмет: ");
+string subject = Console.ReadLine();
+Console.WriteLine($"Отлично! {firstName} любит {subject}.");
+
